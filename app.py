b69a6a22977dea225ccee700513f590bc971ba48
coldrain-f <intmain__@naver.com>
2022-05-09 22:16:25 +0900

    추가 - 로그인 HTML, CSS 기본 설정 변경사항: Bulma CSS 파일 import 및 폰트 어썸 CSS import 완료하였음.

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login_view():
+    return render_template(('/mountain/login.html'))
+
+
 if __name__ == '__main__':
     app.run()
