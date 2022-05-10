a039252fa0c870f8869bcefd4ac1857108596e35
beomjin96 <wkdrnsxv@gmail.com>
2022-05-10 09:26:19 +0900

    list 파일 1차 제작 add파일과 login 파일 테스트로 제작

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return 'Hello World!'
+def main():
+    return render_template("mountain/list.html")
 
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template("mountain/login.html")
+
+@app.route('/add')
+def add():
+    return render_template("mountain/add.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', port=5000, debug=True)
